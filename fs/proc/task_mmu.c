0472330e4a7337dd25926714caac936b5f9fff70
simonpunk <16465163+simonpunk@users.noreply.github.com>
2024-04-04 09:20:24 +0800

    Import simonpunk's SUSFS 1.1.0
    Taken from: https://gitlab.com/simonpunk/susfs4ksu
    Mirror: https://github.com/backslashxx/susfs4ksu
    
    susfs bump to 1.1.0
    
    fix try_umount() not working, should let kernelSU handle the uids.
    Revert hardcoded KSU_GIT_VERSION in Makefile due to testing
    make ksu_susfs tool statically linked as android_log is not needed. Also change [APP_PLATFORM] to [latest] to let user build their own target API level
    Add missing path_umount in mount.h, also no need to export symbol
    Fix kstat spoofing overflowed value. Remove spoofing non-necessary member. Added new feature: add_suspicious_kstat_statically
    Fix a fatal mistake that breaks on displaying process map files
    Fix wrong spoofed dev number in maps
    changes spoofed uid to >= 2000
    update usage of ksu_susfs tool and binarys
    Update README.md
    First release version - 1.0.0
    Change ksu_susfs installation path to /data/adb/ksu/bin/ so that user in a root shell can easily just run it without absolute path
    Add some more examples in posf-fs-data.sh
    bump version to 1.0.1
    Add enable_log feature, user can enable/disable susfs logging to kernel now using ksu_susfs tool
    bump version to 1.1.0
    
    Taken from: https://gitlab.com/simonpunk/susfs4ksu
    Mirror: https://github.com/backslashxx/susfs4ksu

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -26,6 +26,10 @@
 #include <asm/tlbflush.h>
 #include "internal.h"
 
+#if defined(CONFIG_KSU) && defined(CONFIG_KSU_SUSFS)
+#include <linux/susfs.h>
+#endif
+
 void task_mem(struct seq_file *m, struct mm_struct *mm)
 {
 	unsigned long text, lib, swap, ptes, pmds, anon, file, shmem;
@@ -370,6 +374,9 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma, int is_pid)
 
 	start = vma->vm_start;
 	end = vma->vm_end;
+#if defined(CONFIG_KSU) && defined(CONFIG_KSU_SUSFS)
+	if (susfs_suspicious_maps(ino, &ino, &dev)) return;
+#endif
 	show_vma_header_prefix(m, start, end, flags, pgoff, dev, ino);
 
 	/*
